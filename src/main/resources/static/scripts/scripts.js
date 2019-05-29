3361ac24d4ff2dcae5df5ab89024651ecc36d7e7
CryzPark <cryz-park.f@live.com.mx>
2019-05-29 16:18:36 -0500

    update html front 100% rial no feik v2.0

--- src/main/resources/static/scripts/scripts.js
+++ src/main/resources/static/scripts/scripts.js
@@ -8,13 +8,11 @@ $(document).ready(function(){
     var precioPS = "precioP"+idSelect;
     var imgPS = "imgP"+idSelect;
     document.getElementById("nombrePS").innerHTML = document.getElementById(nombrePS).textContent;
-    document.getElementById("precioPS").innerHTML = document.getElementById(precioPS).textContent;
+    document.getElementById("precioPS").innerHTML = "$"+document.getElementById(precioPS).textContent;
     document.getElementById("imgPS").src = document.getElementById(imgPS).src;
     document.getElementById("descripcionPS").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, labore numquam pariatur fugit harum voluptate quisquam saepe possimus provident, minus nemo nisi dolore magnam ducimus, fuga expedita vel explicabo perferendis non ex.";
-    document.getElementById("boxAddCarrito").innerHTML = "<p> <label>Cantidad:</label> <input type='text' placeholder='1' class='form-control' id='txtPrecio"+idSelect+"' width='5'> </p> <p> <a class='btn btn-primary btn-xs' href='/carrito/"+idSelect+"'>Agregar</a> </p>";
+    document.getElementById("boxCantPanes").innerHTML = "<table> <tbody> <tr> <td colspan='3'>Cantidad:</td> </tr> <tr> <td> <input type='button' value='+' id='btnSuma' class='btn btn-secondary'> </td> <td> <input type='text' id='txtCantidad"+idSelect+"' placeholder='1' class='form-control'> </td> <td> <input type='button' value='-' id='btnSuma' class='btn btn-secondary'> </td> </tr> </tbody> </table>";
+    document.getElementById("boxAddCarrito").innerHTML = "<p> <a class='btn btn-primary btn-xs' href='/carrito/"+idSelect+"'>Agregar</a> </p>";
 
   });
-
-
-
 });
