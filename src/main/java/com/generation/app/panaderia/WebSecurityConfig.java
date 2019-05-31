1b0a0f890ce554e065745541587b99fa4423b883
Guillermo <luisg1.luis@gmail.com>
2019-05-30 20:24:38 -0500

    login registro y usuarios v5 solucion de scripts

--- src/main/java/com/generation/app/panaderia/WebSecurityConfig.java
+++ src/main/java/com/generation/app/panaderia/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,13 +26,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/", "/login", "/logout","/registration").permitAll();
+        http.authorizeRequests().antMatchers("/", "/login", "/logout","/registration","/categorias/**","/css/**","/img/**","/scripts/**").permitAll();
 
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").defaultSuccessUrl("/index")
+                .loginPage("/login").defaultSuccessUrl("/",true)
                 .permitAll()
                 .and()
                 .logout()
